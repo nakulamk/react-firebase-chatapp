63da057ae551581eedfa16547941040f431b4eec
Nakula Gowda <nakulamk@gmail.com>
2023-03-31 18:32:00 +0530

    secondary changes

---
+++ src/components/Chats.jsx
@@ -0,0 +1,17 @@
+import React, { useContext, useEffect, useState } from "react";
+
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src="" alt="" />
+        <div className="userChatInfo">
+          <span>ding dong</span>
+          <p>asdf</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
